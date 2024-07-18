430c267ad3b94d0866bf54dd0c0ce8cf07aa00aa
bebop831 <bebop831@thunk>
2024-07-18 08:00:05 -0700

    Check some items of the list

--- snumb.py
+++ snumb.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-import argparse, io
+import argparse, io, tabulate, magic
 from impacket.smbconnection import SMBConnection, SessionError
+from colorama import Fore, Back, Style
 
-def list_files_in_directory(conn : SMBConnection, share_name, path, fs):
+
+def list_share_path(conn : SMBConnection, share_name : str, path : str, fs : str, file_size : int):
     try:
         # List contents of the current directory
         files = conn.listPath(share_name, f"{path}/*")
@@ -11,20 +13,30 @@ def list_files_in_directory(conn : SMBConnection, share_name, path, fs):
             filename = file.get_longname()
             if filename not in [".", ".."]:  # Skip special directories
                 new_path = f"{path}/{filename}"
+                full_path = ("\\\\"+conn.getRemoteHost() + "\\"+share_name + new_path).replace("/", "\\")
                 
                 # If it's a directory, recurse into it
                 if file.is_directory():
-                    list_files_in_directory(conn, share_name, new_path, fs)
+                    list_share_path(conn, share_name, new_path, fs, file_size)
                 else:
+                    #do something with file size here?
                     fs.append(new_path)
-                    contents = read_file_contents(conn, share_name, new_path)
-                    # Do some things with file contents
+                    cur_file_size_mb = file.get_filesize() / 1_000_000 #Convert bytes to megabytes 
+                    if(cur_file_size_mb <= file_size):
+                        contents = read_file_contents(conn, share_name, new_path)
+                        # Do some things with file contents
+                        # parse_file_contents(full_path, contents)
+                        mime = magic.from_buffer(contents)
+                        if "empty" not in mime:
+                            print(full_path, ",",mime)
+                    else:
+                        print(f"{full_path} is too large. {cur_file_size_mb}MB")
 
     except SessionError as se:
         pass
 
     except Exception as e:
-        print(f"Error accessing {path}: {e}")
+        print(f"Error accessing {path}\n{e}")
 
 def read_file_contents(conn, share, path):
     file_obj = io.BytesIO()
@@ -33,8 +45,8 @@ def read_file_contents(conn, share, path):
     return file_obj.read()
 
 
-def parse_file_contents(file_contents):
-    pass
+def parse_file_contents(file_path, file_contents):
+    print(f"file: {file_path}")
 
 def is_readable(smb_con : SMBConnection, shareName : str, path : str = "\*"):
     try:
@@ -55,8 +67,15 @@ def is_writable(smb_con : SMBConnection, shareName : str):
 def enumerate_share(smb_con : SMBConnection, shareName : str):
     pass
 
+def move_cursor(row, column):
+  # Move cursor to specified position
+  print(f"\033[{row};{column}H", end="")
+
+
 def printShareNames(smb_con : SMBConnection):
-    print("Share           Permissions           Comment")
+    share_headers = ["Share", "Perms", "Comment"]
+    shares = []
+
     for x in smb_con.listShares():
         perms = ""
         shareName = x['shi1_netname'][:-1]
@@ -68,12 +87,11 @@ def printShareNames(smb_con : SMBConnection):
         if is_writable(smb_con, shareName):
             perms += "W"
 
-        
+        shares.append([shareName, Fore.GREEN+perms+Style.RESET_ALL, remarks])    
 
-        
-        
-        print(f"{shareName}               {perms}              {remarks}")
 
+    print(tabulate.tabulate(shares, share_headers, colalign=['left', 'center', 'center']))
+        
 def main():
 
     parser = argparse.ArgumentParser(prog="snumb", usage="./snumb.py -i <IP> -p <PORT>")
@@ -83,6 +101,7 @@ def main():
     parser.add_argument("-o", "--output", dest="outfile", required=False, default=None, help="Results will be written to this file.")
     parser.add_argument("-d", "--domain", dest="domain", required=False, default="", help="domain name for Kerberos login.")
     parser.add_argument("-s", "--share", dest="shareName", required=False, default="", help="Share name to enumerate.")
+    parser.add_argument("-f", "--file-size", dest="fileSize", required=False, default=50, type=int, help="Max file size in MB for file enum, inclusive. Default 50MB.")
     parser.add_argument("-a", action="store_true", help="anonymous login", default=False)
 
 
@@ -103,8 +122,8 @@ def main():
         except SessionError:
             print("Login Failed")
             return
-        except:
-            print(f"Connection to {args.addr} could not be established.")
+        except Exception as e:
+            print(f"Connection to {addr_port[0]}:{addr_port[1]} could not be established.")
             return
 
         if smb_con.isGuestSession():
@@ -132,11 +151,13 @@ def main():
             
         files = []
         enumerate_share(smb_con=smb_con, shareName=args.shareName)
-        list_files_in_directory(conn=smb_con, share_name=args.shareName, path="", fs=files)
+        print(f"[+] Max filesize: {args.fileSize}MB")
+        list_share_path(conn=smb_con, share_name=args.shareName, path="", fs=files, file_size=args.fileSize)
+            
         print(files)
-
         
         smb_con.close()
+
 
 main()
 
